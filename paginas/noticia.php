14e9dfcdda997cddecce02819470ab3de7aba116
L0ki1 <parrasantiago.ja@gmail.com>
2022-07-17 07:48:16 -0500

    fb

--- paginas/noticia.php
+++ paginas/noticia.php
@@ -69,7 +69,7 @@
 
     </div>
     
-    <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small"><a id="afbb" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Compartir</a></div>
+    <?php include('../componentes/noticia/fb.php') ?>
 
 
     <footer style="padding-bottom: 90px;">
