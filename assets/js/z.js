798177fffc04d9557ef0411ecf45b713a88b3468
L0ki1 <parrasantiago.ja@gmail.com>
2022-07-17 07:44:43 -0500

    shared

--- assets/js/z.js
+++ assets/js/z.js
@@ -11,7 +11,20 @@ $( document ).ready(function() {
         $("#contenido").show();
 
     }, 1000);
-    $("#fbb").attr("data-href",'http://localhost' + encodeURIComponent(location.href)); 
 
 });
 
+$('.fb-share-button').click(function(e){
+    e.preventDefault();
+    FB.ui(
+    {
+    method: 'feed',
+    name: 'This is the content of the "name" field.',
+    link: 'http://www.groupstudy.in/articlePost.php?id=A_111213073144',
+    picture: 'http://www.groupstudy.in/img/logo3.jpeg',
+    caption: 'Top 3 reasons why you should care about your finance',
+    description: "What happens when you don't take care of your finances? Just look at our country -- you spend irresponsibly, get in debt up to your eyeballs, and stress about how you're going to make ends meet. The difference is that you don't have a glut of taxpayers…",
+    message: ""
+    });
+    });
+    
--- componentes/noticia/fb.php
+++ componentes/noticia/fb.php
@@ -2,9 +2,11 @@
 
 $Url = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
 $Url .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+$Url= urlencode($Url);
+echo $Url;
 
-echo '<div class="fb-share-button" 
-data-href="https://www.your-domain.com/your-page.html" 
-data-layout="button_count">
-</div>';
+echo '<div class="fb-share-button" data-href="'.$Url.'" data-layout="button_count" data-size="small">
+
+<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='.$Url.'&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+';
 ?>
