eda7a342a8888a04e079723246dc0af53129895e
L0ki1 <parrasantiago.ja@gmail.com>
2022-07-18 12:14:40 -0500

    url

--- componentes/noticia/fb.php
+++ componentes/noticia/fb.php
@@ -1,12 +1,11 @@
 <?php
 
-$Url = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
-$Url .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+$Url = "https%3A%2F%2Feldiariodeteziutlan.com%2Fpaginas%2Fnoticia.php%3Fnota%3";
 $Url= urlencode($Url);
 echo $Url;
 
 echo '<div class="fb-share-button" data-href="'.$Url.'" data-layout="button_count" data-size="small">
 
-<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='.$Url.'&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='.$Url.';src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
 ';
 ?>
